6ebfd72b9087ec21be9756840b16348fee80997d
wtt <conquer1ss@gmail.com>
2022-08-10 21:20:57 +0800

    测试微服务的接口

---
+++ yingxue-videos/src/main/java/com/cqupt/controller/DemoController.java
@@ -0,0 +1,18 @@
+package com.cqupt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author kioo
+ */
+@RestController
+@RequestMapping("demos")
+public class DemoController {
+
+    @GetMapping
+    public String demos(){
+        return "videos demos";
+    }
+}
