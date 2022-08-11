c63d103f2ebaebd2323b90d4f930d126a241483d
wtt <conquer1ss@gmail.com>
2022-08-11 14:24:11 +0800

    mysql

---
+++ yingxue-administrators/src/main/java/com/cqupt/controller/AdminController.java
@@ -0,0 +1,13 @@
+package com.cqupt.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author kioo
+ */
+@RestController
+@RequestMapping("admin")
+public class AdminController {
+
+}
