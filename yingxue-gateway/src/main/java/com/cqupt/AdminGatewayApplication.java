5188fd196441083d507fc03604e652ca73af75c3
wtt <conquer1ss@gmail.com>
2022-08-11 09:47:12 +0800

    配置网关

---
+++ yingxue-gateway/src/main/java/com/cqupt/AdminGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.cqupt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kioo
+ */
+@SpringBootApplication
+public class AdminGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminGatewayApplication.class,args);
+    }
+}
